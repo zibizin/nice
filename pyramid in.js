3bbc910cc0a16e69ea34203aaf9376d2fb25f6c3
Daniel Lima dos Santos <Dan_br@outlook.com>
2025-04-07 21:43:58 -0300

    Create pyramid in.js

---
+++ pyramid in.js
@@ -0,0 +1,26 @@
+here the code for building a pyramid generator in javascript
+-------------------------------
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = true;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
